8c8d5040a3dbc464727b7c776f959a07e41c0acf
clintcabanero@gmail.com <clintcabanero@gmail.com>
2015-05-23 13:00:56 -0700

    adding hello world snippet

---
+++ HelloWatch/HelloWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWatch
+//
+//  Created by Clint Cabanero on 5/23/15.
+//  Copyright (c) 2015 Big Leaf Mobile LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ios-watchkit-snippets
-Code snippets for programming with WatchKit - Apple Watch
+
+###Description
+Code snippets using WatchKit for making Apple Watch Apps.
+
+###Folder: HelloWatch
+* A Simple 'Hello World' Apple Watch app.
+
+###Folder: Button
+* Creating a label as an IBOutlet.
+* Creating an IBAction for button tap.
+
+
+
